b8f2b7423df107452a594623448a07f101425395
estebanmg27 <esteban.gonzalez.27.86@gmail.com>
2023-07-05 01:33:31 -0300

    se agrega la funcionalidad para buscar productos

---
+++ js/producto-buscar.js
@@ -0,0 +1,15 @@
+document.addEventListener('keyup', e => {
+    if(e.target.matches('#buscador')){
+        document.querySelectorAll('.prod').forEach(producto => {
+        //    if ((producto.textContent.toLocaleLowerCase().includes(e.target.value)) == true){
+        //         producto.classList.remove('filtro')
+        //    } else {
+        //         producto.classList.add('filtro')
+        //    }
+        
+           producto.textContent.toLocaleLowerCase().includes(e.target.value)
+           ? producto.classList.remove('filtro')
+           : producto.classList.add('filtro');
+        })
+    }
+})
